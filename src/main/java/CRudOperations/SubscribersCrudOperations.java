973b1269d46696a69ddc9425b5a5553269df4f5c
gitdeverstein <hei.andy.8@gmail.com>
2023-11-18 15:20:11 +0300

    create CrudOperations interface for generic CRUD operations

---
+++ src/main/java/CRudOperations/SubscribersCrudOperations.java
@@ -0,0 +1,28 @@
+package CRudOperations;
+
+import Interface.CrudOperations;
+import Model.Subscribers;
+
+import java.util.List;
+
+public class SubscribersCrudOperations implements CrudOperations<Subscribers> {
+    @Override
+    public List<Subscribers> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Subscribers> saveAll(List<Subscribers> toSave) {
+        return null;
+    }
+
+    @Override
+    public Subscribers save(Subscribers toSave) {
+        return null;
+    }
+
+    @Override
+    public Subscribers delete(Subscribers toDelete) {
+        return null;
+    }
+}
